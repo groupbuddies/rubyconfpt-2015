7ac7708e83120b3b831ed70daedefb9d412dece0
Miguel Palhas <mpalhas@gmail.com>
2015-04-07 20:04:02 +0100

    WIP [ci skip]

--- app/js/scene.js
+++ app/js/scene.js
@@ -1,3 +1,17 @@
-// $(function() {
-//   $('#SceneOverlay').fadeIn();
-// })
+$(window).load(function() {
+  var body = document.getElementsByTagName('body');
+  var $viewport = $("html, body");
+
+  $viewport.bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(e){
+    // This identifies the scroll as a user action, stops the animation, then unbinds the event straight after (optional)
+    if ( e.which > 0 || e.type === "mousedown" || e.type === "mousewheel" || e.type === "touchstart"){
+      $(".Scene-anchor").velocity("stop");
+      $viewport.stop().unbind('scroll mousedown DOMMouseScroll mousewheel keyup');
+    }
+  });
+
+  $(".Scene-anchor").velocity("scroll", {
+    duration: 2000,
+    easing: 'easeInOut'
+  }, 2000);
+})
