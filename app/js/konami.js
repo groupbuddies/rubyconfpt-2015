24875af9bf242992c9460c0fadcd22cb801460ff
João Justo <jpjustonunes@hotmail.com>
2015-04-15 17:46:45 +0100

    Adds konami code to rubyconf site

---
+++ app/js/konami.js
@@ -0,0 +1,13 @@
+$(function() {
+  var musicURL = 'music.mp3';
+  var awesomeMusic = new Konami();
+
+  awesomeMusic.code = function() {
+    console.log('THEY ARE TAKING THE HOBITS TO ISENGARD!');
+    var audio = new Audio(musicURL);
+    audio.play();
+  };
+
+
+  awesomeMusic.load();
+});
